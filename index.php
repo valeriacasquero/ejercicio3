926f3f79e48b75388f7a72620d217da5d860dc99
valeriacasquero <valeriacasquero@gmail.com>
2024-04-19 21:44:49 -0300

    fd

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php   
+$numero1 = $_GET ['n1'];      
+$numero2 = $_GET['n2']; 
+$resultado = $numero1+$numero2;
+
+echo 'La suma del número ' .$numero1.' y del número ' .$numero2. ' es igual a '. $resultado;
+
